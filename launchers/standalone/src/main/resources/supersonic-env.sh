5a76a79686afb024636e2c529be9020b151446ad
ningxia202109 <90520833+ningxia202109@users.noreply.github.com>
2024-06-27 01:26:03 +0000

    change mysql password

--- launchers/standalone/src/main/resources/supersonic-env.sh
+++ launchers/standalone/src/main/resources/supersonic-env.sh
@@ -5,8 +5,12 @@
 #       which limits 1000 tokens per request.
 # OPENAI_API_BASE=http://langchain4j.dev/demo/openai/v1
 # OPENAI_API_KEY=demo
-OPENAI_API_BASE=https://api.openai.com/v1
-OPENAI_API_KEY=$OPENAI_API_KEY
+OPENAI_API_BASE=${OPENAI_API_BASE}
+OPENAI_API_KEY=${OPENAI_API_KEY}
 OPENAI_MODEL_NAME=gpt-3.5-turbo
 OPENAI_TEMPERATURE=0.0
 OPENAI_TIMEOUT=PT60S
+
+
+# MYSQL env
+MYSQL_PW=${OPENAI_APIMYSQL_PW_KEY}
