c6be93af13b358b64388c97ee494642d66a1b179
ningxia202109 <90520833+ningxia202109@users.noreply.github.com>
2024-06-27 01:11:21 +0000

    change configure for local

--- launchers/standalone/src/main/resources/supersonic-env.sh
+++ launchers/standalone/src/main/resources/supersonic-env.sh
@@ -3,8 +3,10 @@
 # Replace with your LLM configs
 # Note: The default API key `demo` is provided by langchain4j community
 #       which limits 1000 tokens per request.
-OPENAI_API_BASE=http://langchain4j.dev/demo/openai/v1
-OPENAI_API_KEY=demo
+# OPENAI_API_BASE=http://langchain4j.dev/demo/openai/v1
+# OPENAI_API_KEY=demo
+OPENAI_API_BASE=https://api.openai.com/v1
+OPENAI_API_KEY=$OPENAI_API_KEY
 OPENAI_MODEL_NAME=gpt-3.5-turbo
 OPENAI_TEMPERATURE=0.0
 OPENAI_TIMEOUT=PT60S
